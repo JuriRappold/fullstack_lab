3e665f4e221666ef104562864a8e8c15560d61f7
JuriRappold <juri@rappold.berlin>
2026-08-11 17:11:25 +0200

    independent project structure of utils & backend (package&tsconfig configurations) and setup a http server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,13 @@
 // just for testing the utils import
 import { testing } from "@fullstack-lab/utils";
-testing()
+import { app } from './app.js'
+import * as http from "node:http";
+
+const PORT = process.env.PORT || 3001;
+
+
+const httpServer = http.createServer(app);
+httpServer.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+// testing()
